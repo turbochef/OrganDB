fd07c6f01902e055ab53219ef06b29a06997aa0f
Daniel R. Soetanto <turbochef.ds@gmail.com>
2015-04-01 23:13:34 -0500

    added html pages to contain login scripts

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
-$connection = mysql_connect('localhost', 'maddutch_laur123', 'ilovedaniel');
+$connection = mysql_connect('localhost', 'maddutch_lauren', 'ilovedaniel');
 if (!$connection){
     die("Database Connection Failed" . mysql_error());
 }
-$select_db = mysql_select_db('test');
+$select_db = mysql_select_db('maddutch_laur123');
 if (!$select_db){
     die("Database Selection Failed" . mysql_error());
 }
